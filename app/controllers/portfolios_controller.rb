98f5d100a74a507f447d439190ff5b119057fef3
Monica Howard <jay.m.howard@gmail.com>
2018-04-07 19:41:37 -0500

    Adds actions in portfolio directory

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,6 @@
 class PortfoliosController < ApplicationController
   def index
-    @portolio_items = Portfolio.all
+    @portfolio_items = Portfolio.all
   end
 
   def new
@@ -17,6 +17,26 @@ class PortfoliosController < ApplicationController
         format.html { render :new }
       end
     end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 end
