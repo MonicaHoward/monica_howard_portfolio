9d07313a3b24f183f3709cd403d430ca2433f70e
Monica Howard <jay.m.howard@gmail.com>
2018-05-16 21:39:08 -0500

    Adds installation requirements for petergate gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180421164240) do
+ActiveRecord::Schema.define(version: 20180517022953) do
 
   create_table "friendly_id_slugs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "slug", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20180421164240) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
