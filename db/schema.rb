1defddd62decdac66cf0307e4a4adf22be4171b4
Monica Howard <jay.m.howard@gmail.com>
2018-05-29 00:31:30 -0500

    Updates portfolio controller to include attrs for position

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517022953) do
+ActiveRecord::Schema.define(version: 20180529041442) do
 
   create_table "friendly_id_slugs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "slug", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20180517022953) do
     t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
   end
 
   create_table "skills", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
